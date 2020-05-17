be0507ee0a48d9c999432baa45148281eba4f440
vagrant <vagrant@localhost.localdomain>
2020-05-17 13:16:55 +0000

    [Finish] task-3 No.2

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,25 @@
 class BookCommentsController < ApplicationController
 	def create
 		@book = Book.find(params[:book_id])
-		@comment = current_user.book_comments.new(book_comment_params)
-		@comment.book_id = @book.id
-	    @comment.save
-	    redirect_to book_path(@book)
+		@book_comment = current_user.book_comments.new(book_comment_params)
+		@book_comment.book_id = @book.id
+	    if @book_comment.save
+	       redirect_to book_path(@book)
+	    else
+            @user = @book.user
+	    	render template: "books/show"
+	    end
 	end
 
 	def destroy
 		@book = Book.find(params[:book_id])
-		@comment = BookComment.find(params[:id])
-		@comment.destroy
-		redirect_to book_path(@book)
+		@comment = BookComment.find(params[:book_id])
+		if @comment.user_id == current_user.id
+		   @comment.destroy
+		   redirect_to book_path(@book)
+		else
+			redirect_to book_path(@book)
+		end
 	end
 	private
 	def book_comment_params
